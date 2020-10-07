a5969fef517436e9da01b74261da10675be5b7af
XX-J <XX-J@mail.ru>
2020-10-08 00:11:47 +0300

    Add files via upload

---
+++ Исправления AliExpress v1.5.user.js
@@ -0,0 +1,36 @@
+﻿// ==UserScript==
+// @name           Amendments for AliExpress
+// @name:ru        Исправления AliExpress
+// @version        1.5
+// @description    Replacement of Russian links to the English site of their English-speaking counterparts. Disabling autotranslate comments to the goods in the English language.
+// @description:ru Замена русскоязычных ссылок на англоязычном сайте их англоязычными аналогами. Отключение автоперевода комментариев к товарам на английский язык.
+// @icon           https://ae01.alicdn.com/images/eng/wholesale/icon/aliexpress.ico
+// @author         XX-J...
+// @include        *://aliexpress.com/*
+// @include        *://*.aliexpress.com/*
+// @include        *://aliexpress.ru/store/*
+// @include        *://*.aliexpress.ru/store/*
+// ==/UserScript==
+
+function Replace(link) { if (/aliexpress.ru/i.test(link.href)) link.href = decodeURIComponent(link.href.replace('aliexpress.ru/','aliexpress.com/')); }
+function SplitForReplace(links) { for (var i = 0; i < links.length; ++i) Replace(links[i]); }
+
+(function () {
+
+//    Меняем ссылки на англоязычные
+    document.addEventListener('DOMNodeInserted', function(event) {
+        if (!event || !event.target || !(event.target instanceof HTMLElement)) return;
+        if (event.target instanceof HTMLAnchorElement) Replace(event.target);
+        SplitForReplace(event.target.getElementsByTagName('a'));
+    }, { passive: true });
+    SplitForReplace(document.getElementsByTagName('a'));
+
+//    Нужно привести к одному домену для управления основной страницей из фрейма
+    document.domain = 'aliexpress.com';
+
+//    Отключаем перевод отзывов на английский
+    if (/feedback.aliexpress.com/i.test(window.location) && window.parent.document.querySelector('#product-evaluation[marginheight]')) setTimeout(function () {
+        document.querySelector('#cb-translate[checked]').click();
+        window.parent.document.querySelector('#product-evaluation').removeAttribute('marginheight');
+    }, 2500);
+})();
