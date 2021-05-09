41437479d828dc9b05cecbd193aba5b67f9aa426
XX-J <XX-J@mail.ru>
2021-05-09 05:41:50 +0300

    Minor changes..

--- Amendments for AliExpress.user.js
+++ Amendments for AliExpress.user.js
@@ -1,30 +1,36 @@
 
 // ==UserScript==
-// @name:ru        Исправления AliExpress
-// @name           Amendments for AliExpress
-// @version        2.1
-// @description:ru Замена русскоязычных ссылок на англоязычном сайте их англоязычными аналогами. Отключение автоперевода комментариев к товарам на английский язык. Добавление опций отображения 50 и 100 заказов в списке заказов. Изменяемые настройки сохраняются.
-// @description    Replacement of Russian links to the English site of their English-speaking counterparts. Disabling autotranslate comments to the goods in the English language. The addition display options of 50 and 100 orders in the orders list. Changed settings are saved.
-// @icon           https://ae01.alicdn.com/images/eng/wholesale/icon/aliexpress.ico
-// @update         https://github.com/XX-J/Amendments-for-AliExpress/raw/main/Amendments for AliExpress.user.js
-// @author         XX-J...
-// @include        *://aliexpress.com/*
-// @include        *://*.aliexpress.com/*
-// @include        *://aliexpress.ru/store/*
-// @include        *://*.aliexpress.ru/store/*
+// @name:ru         Исправления AliExpress
+// @name            Amendments for AliExpress
+// @version         2.2
+// @description:ru  Замена русскоязычных ссылок на англоязычном сайте их англоязычными аналогами. Отключение автоперевода комментариев к товарам на английский язык. Добавление опций отображения 50 и 100 заказов в списке заказов. Изменяемые настройки сохраняются.
+// @description     Replacement of Russian links to the English site of their English-speaking counterparts. Disabling autotranslate comments to the goods in the English language. The addition display options of 50 and 100 orders in the orders list. Changed settings are saved.
+// @icon            https://ae01.alicdn.com/images/eng/wholesale/icon/aliexpress.ico
+// @update          https://github.com/XX-J/Amendments-for-AliExpress/raw/main/Amendments for AliExpress.user.js
+// @author          XX-J...
+// @include         *://aliexpress.com/*
+// @include         *://*.aliexpress.com/*
+// @include         *://aliexpress.ru/store/*
+// @include         *://*.aliexpress.ru/store/*
 // ==/UserScript==
 
 
 //    Меняем ссылки на англоязычные
 
-function Replace(link) { if (/aliexpress.ru/i.test(link.href)) link.href = decodeURIComponent(link.href.replace('aliexpress.ru/','aliexpress.com/')); };
-function SplitForReplace(links) { for (var i = 0; i < links.length; ++i) Replace(links[i]); };
+function Replace(link) {
+  if (!!link.getAttribute('href')) {
+    link.href = decodeURIComponent(link.href);
+    link.href = link.href.replace('aliexpress.ru/', 'aliexpress.com/');
+  };
+};
+function SplitForReplace(links) { for (var i = 0; i < links.length; ++i) Replace(links[i]) };
 
 document.addEventListener('DOMNodeInserted', function(event) {
   if (!event || !event.target || !(event.target instanceof HTMLElement)) return;
   if (event.target instanceof HTMLAnchorElement) Replace(event.target);
   SplitForReplace(event.target.getElementsByTagName('a'));
 }, { passive: true });
+
 SplitForReplace(document.getElementsByTagName('a'));
 
 
@@ -33,7 +39,7 @@ if (localStorage.translate) {  //  Проверяем доступность DOM
 
 //    Отключаем перевод отзывов на английский
 
-  if (/feedback.aliexpress.com/i.test(window.location)) {
+  if (window.location.hostname.includes('feedback')) {
 
     let Input = document.querySelector('#translate'), Stor = localStorage.translate;
     if (Input.value != Stor) { Input.value = Stor; Input.parentNode.submit(); };
@@ -43,7 +49,7 @@ if (localStorage.translate) {  //  Проверяем доступность DOM
 
 //    Устанавливаем отображение 50-и заказов в списке заказов
 
-  if (/trade.aliexpress.com/i.test(window.location)) {
+  if (window.location.hostname.includes('trade')) {
 
     if (!localStorage.pageSize) localStorage.pageSize = '50';
     let Input = document.querySelector('[name="pageSize"]'), Stor = localStorage.pageSize;
@@ -62,4 +68,3 @@ if (localStorage.translate) {  //  Проверяем доступность DOM
   };
 
 };
-
